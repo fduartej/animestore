a1e68ffa3c7b2d36fd927b3218fba095271a4fb9
Fred Duarte <fduartej@gmail.com>
2025-04-12 09:08:13 -0500

    reusando el navbar

--- js/App.js
+++ js/App.js
@@ -1,30 +1,28 @@
 function App() {
   return (
-    <div>
-      <nav class="navbar">
-        <h1 class="navbar-title">Anime Store</h1>
-        <button class="hamburger" aria-label="Toggle menu">
-          &#9776;
-        </button>
-        <ul class="navitems">
-          <li>
-            <a href="index.html">Catalogo</a>
-          </li>
-          <li>
-            <a href="contacto.html">Contactanos</a>
-          </li>
-          <li>
-            <a href="torneo.html">Torneo</a>
-          </li>
-          <li>
-            <a href="game.html">Game</a>
-          </li>
-          <li>
-            <a href="#">Acerca</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav class="navbar">
+      <h1 class="navbar-title">Anime Store</h1>
+      <button class="hamburger" aria-label="Toggle menu">
+        &#9776;
+      </button>
+      <ul class="navitems">
+        <li>
+          <a href="index.html">Catalogo</a>
+        </li>
+        <li>
+          <a href="contacto.html">Contactanos</a>
+        </li>
+        <li>
+          <a href="torneo.html">Torneo</a>
+        </li>
+        <li>
+          <a href="game.html">Game</a>
+        </li>
+        <li>
+          <a href="#">Acerca</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
